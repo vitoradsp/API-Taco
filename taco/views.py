6299b212175a377cfd3f91eaff138379ff338da6
Vitor <vitoradsp@gmail.com>
2022-06-08 18:29:32 -0300

    fix

--- taco/views.py
+++ taco/views.py
@@ -1,8 +1,7 @@
-from rest_framework import mixins, permissions, viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets
 from taco.models import Alimento
 from taco.serializers import AlimentoSerializer
-import sqlite3
+
 
 class AlimentoViewSet(viewsets.ModelViewSet):
     queryset = Alimento.objects.all()
